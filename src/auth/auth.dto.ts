eae84487b81bef0d7722d9164387d903e368ec6a
Ashraf <nayeemashraf92@gmail.com>
2026-05-21 12:15:37 +0500

    @ApiProperty()

--- src/auth/auth.dto.ts
+++ src/auth/auth.dto.ts
@@ -1,28 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   fullName!: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   fatherName!: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   phone!: string;
 
+  @ApiProperty()
   @IsString()
   @MinLength(6)
   password!: string;
 }
 
 export class LoginDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   phone!: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password!: string;
